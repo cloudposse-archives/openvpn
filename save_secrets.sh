43d397c5c20baa282d6ca628cce77b801e7420e1
Igor Rodionov <goruha@gmail.com>
2016-12-19 18:06:17 +0600

    Update config save secrets config

--- save_secrets.sh
+++ save_secrets.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+NAMESPACE=${NAMESPACE:-default}
+TYPE=${TYPE:-Opaque}
+
 if [ -z "$SECRET_NAME" ]; then
     echo "ERROR: Secret name is empty or unset"
     exit 1
@@ -12,9 +15,9 @@ CA=$(cat $PKI_DIR/ca.crt | base64 --wrap=0)
 CERT=$(cat $PKI_DIR/issued/${EASYRSA_REQ_CN}.crt | base64 --wrap=0)
 DH=$(cat $PKI_DIR/dh.pem | base64 --wrap=0)
 TLS_AUTH=$(cat $PKI_DIR/ta.key | base64 --wrap=0)
+CONFIG=$(cat /etc/openvpn/openvpn.conf | base64 --wrap=0)
+ENV_VARS=$(cat /etc/openvpn/ovpn_env.sh | base64 --wrap=0)
 
-NAMESPACE=${NAMESPACE:-default}
-TYPE=${TYPE:-Opaque}
 
 kubectl get secrets --namespace $NAMESPACE $SECRET_NAME && ACTION=replace || ACTION=create;
 
@@ -31,7 +34,9 @@ cat << EOF | kubectl $ACTION -f -
    "ca": "$CA",
    "cert": "$CERT",
    "dh": "$DH",
-   "tls_auth": "$TLS_AUTH"
+   "tls_auth": "$TLS_AUTH",
+   "config": "$CONFIG",
+   "vars": "$ENV_VARS",
  },
  "type": "$TYPE"
 }
